d8341ba9a054685fcf58a632bc5b1b2ea16997f3
ongunmehmet <ongunmehmet21@gmail.com>
2022-02-11 14:30:54 +0300

    inital message

---
+++ takimruhu/src/main/java/com/takimruhu/TakimruhuApplication.java
@@ -0,0 +1,13 @@
+package com.takimruhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakimruhuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakimruhuApplication.class, args);
+    }
+
+}
---
+++ takimruhu/src/test/java/com/takimruhu/TakimruhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takimruhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakimruhuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

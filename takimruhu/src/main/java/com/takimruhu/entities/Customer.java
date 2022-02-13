f40ace71a09807df0c3fb9c69a2d10de115cfc6b
umutcankaya <umutcan.kaya@bil.omu.edu.tr>
2022-02-13 19:12:29 +0300

    initialized commit

---
+++ takimruhu/src/main/java/com/takimruhu/entities/Customer.java
@@ -0,0 +1,70 @@
+package com.takimruhu.entities;
+
+public class Customer {
+    private int customerId;
+    private String name;
+    private String surName;
+    private String Email;
+    private String password;
+    private String PhoneNumber;
+    private boolean isAdmin;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Customer() {
+    }
+}

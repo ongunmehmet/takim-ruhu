6c60f0eab69a4f7d68b5a3752b3f436590cfd00d
EmirhanTugtekin <emirhann20@gmail.com>
2022-02-15 17:53:07 +0300

    initial commit

---
+++ takimruhu/src/main/java/com/takimruhu/entities/Brand.java
@@ -0,0 +1,18 @@
+package com.takimruhu.entities;
+
+public class Brand {
+    private int brandId;
+    private String brandName;
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+}
